2015d1c2558f06e9132bb193f41677b1e2bb9f3d
nadiyahatkh <nadiyahatikah843@gmail.com>
2024-08-07 12:52:36 +0700

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: '45.64.99.242',
-            port: '8850',
-            pathname: '*',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '45.64.99.242',
+        port: '8850',
+        pathname: '**',
       },
+    ],
+  },
 };
 
 export default nextConfig;
